baa8b2c45fab48a172c95c4478ba87ee91e652f4
Civita2107 <f.civitareale@gmail.com>
2025-02-15 15:45:09 -0600

    Edited demoScheduler

--- Demo/DemoScheduler/demoScheduler.c
+++ Demo/DemoScheduler/demoScheduler.c
@@ -28,18 +28,26 @@ CustomerData_t customers[NUM_CUSTOMERS] = {
     {2, 3, 8, 4},
     {3, 5, 12, 1},
     {4, 1, 7, 1},
-    {5, 4, 15, 5},
+    {5, 4, 15, 2},
 };
 
 void vBarberTask(void *pvParameters) {
-    ( void ) pvParameters;
     CustomerData_t customer;
     for (;;) {
         if (xQueueReceive(xWaitingRoomQueue, &customer, portMAX_DELAY) == pdTRUE) {
-            printf("[Barber] Serving Customer %d\n", customer.id);
+            TickType_t startTime = xTaskGetTickCount();
+            printf("[Barber] Serving Customer %d at time %d ms\n", customer.id, startTime * portTICK_PERIOD_MS);
             xSemaphoreTake(xBarberChair, portMAX_DELAY);
-            vTaskDelay(pdMS_TO_TICKS(customer.serviceTime));
-            printf("[Barber] Finished Customer %d\n", customer.id);
+            
+            // Calculate the delay based on service time and hair length
+            TickType_t serviceDuration = pdMS_TO_TICKS(customer.serviceTime * customer.hairLength);
+            TickType_t currentTick = xTaskGetTickCount();
+            while (currentTick < serviceDuration + startTime) {
+                currentTick = xTaskGetTickCount();
+            }
+            
+            TickType_t endTime = xTaskGetTickCount();
+            printf("[Barber] Finished Customer %d at time %d ms\n", customer.id, endTime * portTICK_PERIOD_MS);
             xSemaphoreGive(xBarberChair);
             servedCustomers++;
         }
@@ -62,11 +70,22 @@ void vCustomerTask(void *pvParameters) {
 
 void vCustomerGeneratorCallback(TimerHandle_t xTimer) {
     int customerIndex = (int)pvTimerGetTimerID(xTimer);
-    xTaskCreate(vCustomerTask, "Customer", configMINIMAL_STACK_SIZE, &customers[customerIndex], tskIDLE_PRIORITY + 1, NULL);
+    UBaseType_t priority = tskIDLE_PRIORITY + 1 + (NUM_CUSTOMERS - customerIndex); // Assign higher priority to earlier customers
+    xTaskCreate(vCustomerTask, "Customer", configMINIMAL_STACK_SIZE, &customers[customerIndex], priority, NULL);
 }
 
 void vCustomerGeneratorTask(void *pvParameters) {
-    ( void ) pvParameters;
+    // Sort customers by service time (Shortest Job First)
+    for (int i = 0; i < NUM_CUSTOMERS - 1; i++) {
+        for (int j = i + 1; j < NUM_CUSTOMERS; j++) {
+            if (customers[i].serviceTime > customers[j].serviceTime) {
+                CustomerData_t temp = customers[i];
+                customers[i] = customers[j];
+                customers[j] = temp;
+            }
+        }
+    }
+
     TimerHandle_t xTimers[NUM_CUSTOMERS];
     for (int i = 0; i < NUM_CUSTOMERS; i++) {
         xTimers[i] = xTimerCreate("CustomerTimer", pdMS_TO_TICKS(customers[i].arrivalTime), pdFALSE, (void *)i, vCustomerGeneratorCallback);
