e0f12b4780b9dffbb5d1f31f9ce34496e51fb21d
luibart <bartolomeoluigi85@gmail.com>
2025-02-16 00:14:23 +0000

    qemu and gdb script fix

--- gdb_script.sh
+++ gdb_script.sh
@@ -1,15 +1,14 @@
 #!/bin/zsh
 
-DEFAULT_DIR="$HOME/Desktop/Polito/CAOS/HaclOSsim/Demo/DemoScheduler/build/gcc"
+DEFAULT_DIR="$HOME/Desktop/Polito/CAOS/HaclOSsim/Demo/DemoScheduler/build/gcc/output"
 PROJECT_DIR="${PROJECT_DIR:-$DEFAULT_DIR}"
-
 
 if [ ! -d "$PROJECT_DIR" ]; then
     echo "Default path not found. Searching on Desktop..."
     SEARCH_PATH=$(find "$HOME/Desktop" -type d -name "HaclOSsim" -print -quit)
     
     if [ -n "$SEARCH_PATH" ]; then
-        PROJECT_DIR="$SEARCH_PATH/Demo/DemoScheduler/build/gcc"
+        PROJECT_DIR="$SEARCH_PATH/Demo/DemoScheduler/build/gcc/output"
         echo "Found project directory at: $PROJECT_DIR"
     else
         echo "Error: Could not find HaclOSsim directory on Desktop."
@@ -18,7 +17,4 @@ if [ ! -d "$PROJECT_DIR" ]; then
 fi
 
 cd "$PROJECT_DIR" || { echo "Directory not found: $PROJECT_DIR"; exit 1; }
-
-make clean
-make
-qemu-system-arm -s -S -M mps2-an385 -cpu cortex-m3 -monitor none -nographic -serial stdio -kernel ./output/RTOSDemo.out
+sudo arm-none-eabi-gdb RTOSDemo.out -ex "target remote localhost:1234"
